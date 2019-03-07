1df8f475303b934b42d6b3f0bdf0a5fc6ecb462a
P.GOEL.HS <purushottamgoel@users.noreply.github.com>
2019-03-06 21:23:01 -0800

    Create convention.cpp

---
+++ Contests/USACO/2018-19/DEC_2018/Silver/convention.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <cstring>
+#include <map>
+#include <cstdlib>
+#include <ctime>
+#include <cassert>
+#include <bitset>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <list>
+#include <stdexcept>
+
+
+using namespace std;
+
+int main ()
+{
+//    std::ios_base::sync_with_stdio(false), cin.tie(0) , cout.tie(0);
+//    freopen("convention.in", "r", stdin);
+//    freopen("convention.out", "w", stdout);
+
+return 0;
+}
+
