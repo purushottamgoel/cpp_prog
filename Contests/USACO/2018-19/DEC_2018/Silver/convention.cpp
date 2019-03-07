79537d53a365239b2f4fdf7e7f6ec697660821d7
P.GOEL.HS <purushottamgoel@users.noreply.github.com>
2019-03-06 23:44:02 -0800

    Update convention.cpp

--- Contests/USACO/2018-19/DEC_2018/Silver/convention.cpp
+++ Contests/USACO/2018-19/DEC_2018/Silver/convention.cpp
@@ -22,12 +22,26 @@
 
 using namespace std;
 
+vector<int> v;
+
 int main ()
 {
 //    std::ios_base::sync_with_stdio(false), cin.tie(0) , cout.tie(0);
 //    freopen("convention.in", "r", stdin);
 //    freopen("convention.out", "w", stdout);
+  int n,m,c,r;
+  cin >> n >> m >> c;
+  for(int i=0;i<n;i++){
+    cin >> x;
+    v.push_back(x);
+  }
+  sort(v.begin(),v.end());
+  r=v.back()-v[0];
+  cout << r;
+  
+  
+    
 
-return 0; 
+  return 0;
 }
 
